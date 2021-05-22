b2321dbdb58a61459b21b065d50572c5746dbadc
Zubair Abubakar <zabubakar@paga.com>
2021-05-22 19:04:56 +0100

    setup library for PyPi deployment

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup(
+    name='paga-collect-api-lib',
+    packages=['paga-collect-api-lib'],
+    version='1.0',
+    license='MIT',
+    description='A helper class/SDK to enable developers easily integrate Paga Collect API',
+    author='Zubair Abubakar',
+    author_email='zubair@zubairabubakar.co',
+    url='https://github.com/Paga-Developer-Community/paga-collect-python-lib',
+    keywords=['payment api', 'paga', 'paga collect api'],
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 5 - Production/stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Payment API',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
